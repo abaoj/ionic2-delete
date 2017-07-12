b0d0089b08b18dcc546e7768ac8e6a9015c12e38
abao123 <liu.baobaoyx@163.com>
2017-07-12 15:46:14 +0800

    列表删除

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,23 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  items;
+  constructor() {
+    this.items = [
+      {title: 'item1'},
+      {title: 'item2'},
+      {title: 'item3'},
+      {title: 'item4'},
+      {title: 'item5'},
+      {title: 'item6'}
+    ];
   }
 
+  removeItem(item){
+    for(var i=0;i<this.items.length;i++){
+      if(this.items[i] == item){
+        this.items.splice(i, 1);
+      }
+    }
+  }
 }
